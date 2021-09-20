2f80c805ae1afd882601128d79a7c1838a8eb9a9
Can202 <mgoopazo@gmail.com>
2021-09-20 13:03:26 -0300

    better

--- src/dumpress.py
+++ src/dumpress.py
@@ -5,27 +5,65 @@ import sys
 from sys import exit
 
 def main():
+	directory = ""
 	if len(sys.argv) > 1:
 		for i in range(len(sys.argv)):
-			if os.path.exists(sys.argv[i]) and i != 0:
-				uncompress(sys.argv[i])
 			
-def uncompress(File):
-	print("uncompress " + File)
+			if sys.argv[i] == "-v" or sys.argv[i] == "--version":
+				print("v0.1")
+				exit()
+			
+			if sys.argv[i] == "-d":
+				if os.path.exists(sys.argv[i+1]):
+					directory = sys.argv[i+1]
+					sys.argv[i+1] = ""
+				else:
+					os.system("mkdir -p " + sys.argv[i+1])
+					if os.path.exists(sys.argv[i+1]):
+						print("dir created")
+						directory = sys.argv[i+1]
+						sys.argv[i+1] = ""
+					else:
+						print("error")
+						exit()
+			if i != 0:
+				if os.path.exists(sys.argv[i]):
+					uncompress(sys.argv[i], directory)
+	else:
+		print("""dumpress
+		Use:
+		dumpress <file>   uncompress
+		dumpress -d <folder> <file> uncompress on folder""")
+			
+def uncompress(File, Dir):
 	END=len(File)
 	if File[END-4:END] == ".tar":
-		os.system("tar -xvf " + File)
+		if Dir != "":
+			Dir = " -C " + Dir
+		os.system("tar -xvf " + File + Dir)
 	elif File[END-7:END] == ".tar.gz" or File[END-4:END] == ".tgz":
-		os.system("tar -xvf " + File)
+		if Dir != "":
+			Dir = " -C " + Dir
+		os.system("tar -xvf " + File + Dir)
 	elif File[END-8:END] == ".tar.bz2" or File[END-4:END] == ".tbz" or File[END-5:END] == ".tbz2" or File[END-4:END] == ".tb2":
-		os.system("tar -xvf " + File)
+		if Dir != "":
+			Dir = " -C " + Dir
+		os.system("tar -xvf " + File + Dir)
 	elif File[END-6:END] == ".tar.Z" or File[END-4:END] == ".taz" or File[END-3:END] == ".tz":
-		os.system("tar -xvf " + File)
+		if Dir != "":
+			Dir = " -C " + Dir
+		os.system("tar -xvf " + File + Dir)
 	elif File[END-7:END] == ".tar.lz" or File[END-9:END] == ".tar.lzma" or File[END-4:END] == ".tlz":
-		os.system("tar -xvf " + File)
+		if Dir != "":
+			Dir = " -C " + Dir
+		os.system("tar -xvf " + File + Dir)
 	elif File[END-7:END] == ".tar.xz" or File[END-4:END] == ".txz":
-		os.system("tar -xvf " + File)
+		if Dir != "":
+			Dir = " -C " + Dir
+		os.system("tar -xvf " + File + Dir)
 	elif File[END-4:END] == ".zip":
-		os.system("unzip " + File)
+		if Dir != "":
+			Dir = " -d " + Dir
+		os.system("unzip " + File + Dir)
 if __name__=="__main__":
 	main()
